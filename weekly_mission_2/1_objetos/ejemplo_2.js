a61662b7a0e12a9075a49f3f903cb4e92b0de0c3
MauricioMunguia10 <mauriciomunguiagarcia@gmail.com>
2022-04-29 17:50:36 -0500

    add: Section 'Objetos'

---
+++ weekly_mission_2/1_objetos/ejemplo_2.js
@@ -0,0 +1,9 @@
+// Ejemplo 2: Crear un objeto con propiedades
+
+const myObjetc2 = {
+    name: "Mauricio",
+    age: 20
+  }
+
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObjetc2)
