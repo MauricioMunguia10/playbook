71e06fc55e5fc02bf9bf79a36097631f2472983e
MauricioMunguia10 <mauriciomunguiagarcia@gmail.com>
2022-04-29 18:05:29 -0500

    add: Section 'Operadores'

---
+++ weekly_mission_2/2_operadores/ejemplo_3.js
@@ -0,0 +1,10 @@
+// Ejemplo 3: forEach para imprimir los países en letras mayúsculas
+
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+console.log("Ejemplo 5: Imprimiendo la lista de países en mayúsculas")
+countries.forEach((element) => console.log(element.toUpperCase()))
+
+//Y en minusculas
+
+// console.log("Ejemplo 5.1: Imprimiendo la lista de países en minusculas")
+// countries.forEach((element) => console.log(element.trCase()))
